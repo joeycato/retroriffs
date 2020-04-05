fd3a808d9449c8a1d267bde2e1888ae4746e78b3
joey <joeycato@gmail.com>
2020-04-05 14:31:30 -0700

    Final updates

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Follow } from "react-twitter-widgets";
+import styles from "./Bio.module.scss";
+import config from "../../data/SiteConfig";
 
-const About = () => (
-  <>
-    <h1>Edit About component or pages/about.js to include your information.</h1>
-  </>
-)
+const About = ({ expanded }) => (
+    <>
+        <img
+            className={styles.avatar}
+            src={config.userAvatar}
+            alt={config.userName}
+        />
+        <p>
+            Hi! I'm <strong>{config.userName}</strong>. Thanks for stopping by
+            and entertaining my random thoughts. <br />
+            <br></br>
+            {` `}
+            <Follow
+                username={config.userTwitter}
+                options={{ count: expanded ? true : "none" }}
+            />
+        </p>
+    </>
+);
 
-export default About
+export default About;
